85ff3aefcc9dbd63babc4f1b018cc6076acbe81c
Jan Nehring <jan.nehring@dfki.de>
2022-03-08 19:25:56 +0100

    add ui shutdown hook

--- src/cad/ui/evolution_ui.py
+++ src/cad/ui/evolution_ui.py
@@ -6,6 +6,7 @@ from cad.common.app import App
 import threading
 import logging
 import time
+import atexit
 
 # readable string representation of time in seconds 
 def format_time(t):
@@ -61,18 +62,6 @@ class EvolutionUI:
         if not App.get_config()["hide_ui"]:
             App.subscribe("generation_started", generation_started)
 
-        # subscrube to iteration_finished event
-        # def iteration_finished(i_iteration):
-        #     n_iterations=App.get_context("n_generation_size") * App.get_config()["n_poolsize"]
-        #     i_iterations=App.get_context("i_iteration")
-        #     self.infos["iteration"]=f"{i_iterations}/{n_iterations}"
-        #     time_elapsed=time.time()-self.start_time
-        #     self.infos["time elapsed"] = format_time(time_elapsed)
-        #     self.info_window.update_dict(self.infos)
-        #     self.ui.display()
-        # if not App.get_config()["hide_ui"]:
-        #     App.subscribe("iteration_finished", iteration_finished)
-
         def update_ui_timer():
             while not self.ui.killed:
                 try:
@@ -94,10 +83,13 @@ class EvolutionUI:
             self.update_ui_thread.start()
 
         # shut down when pipeline is finished
-        def pipeline_finished():
+        def shutdown():
             self.ui.end()
         if not App.get_config()["hide_ui"]:
-            App.subscribe("pipeline_finished", pipeline_finished)
+            App.subscribe("pipeline_finished", shutdown)
+
+        # register a shutdown hook to close ui when application closes
+        atexit.register(shutdown)
 
     def update(self):
 
