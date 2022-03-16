a89c6311778f40f57534305ed2a792be675dc89d
Jan Nehring <jan.nehring@dfki.de>
2022-03-16 17:12:43 +0100

    update evolutions

--- src/cad/evo/evolve_mbeya.py
+++ src/cad/evo/evolve_mbeya.py
@@ -24,47 +24,82 @@ try:
 
     class MbeyaLoss(LossFunction):
 
+        # fundamental: note number of the fundamental
+        # add_octave: the first toot is one octave above the fundamental
+        # scale: define the scale of the toots of the didgeridoo as semitones relative from the fundamental
+        # target_peaks: define the target peaks as list of math.log(frequency, 2). overrides scale 
+        # weights: override the default weights
+        # {
+        #     "tuning_loss": 8,
+        #     "volume_loss": 0.5,
+        #     "octave_loss": 4,
+        #     "n_note_loss": 5,
+        #     "diameter_loss": 0.1,
+        #     "fundamental_loss": 8,
+        # }
         def __init__(self):
-            LossFunction.__init__(self)
+            LossFunction.__init__(self, fundamental=-31, add_octave=True, scale=[0,2,3,5,7,9,10], target_peaks=None, weights={})
 
-            self.scale=[0,2,3,5,7,9,10]
-            self.fundamental=-31
+            self.weights={
+                "tuning_loss": 8,
+                "volume_loss": 0.5,
+                "octave_loss": 4,
+                "n_note_loss": 5,
+                "diameter_loss": 0.1,
+                "fundamental_loss": 8,
+            }
+            for key, value in weights.items():
+                if key not in self.weights:
+                    raise Exception(f"Unknown weight {key}")
+                self.weights[key]=value
 
-            self.scale_note_numbers=[]
-            for i in range(len(self.scale)):
-                self.scale_note_numbers.append(self.scale[i]+self.fundamental)
 
-            n_octaves=10
-            self.target_peaks=[]
-            for note_number in self.scale_note_numbers:
-                for i in range(0, n_octaves):
-                    transposed_note=note_number+12*i
-                    freq=note_to_freq(transposed_note)
-                    freq=math.log(freq, 2)
-                    self.target_peaks.append(freq)
+            self.scale=scale
+            self.fundamental=fundamental
+            self.add_octave=add_octave
+
+            if self.target_peaks is not None:
+                self.target_peaks=target_peaks
+            else:
+                self.scale_note_numbers=[]
+                for i in range(len(self.scale)):
+                    self.scale_note_numbers.append(self.scale[i]+self.fundamental)
+
+                n_octaves=10
+                self.target_peaks=[]
+                for note_number in self.scale_note_numbers:
+                    for i in range(0, n_octaves):
+                        transposed_note=note_number+12*i
+                        freq=note_to_freq(transposed_note)
+                        freq=math.log(freq, 2)
+                        self.target_peaks.append(freq)
 
         def get_loss(self, geo, context=None):
 
-            fundamental=single_note_loss(-31, geo)*4
-            octave=single_note_loss(-19, geo, i_note=1)
+            fundamental=single_note_loss(-31, geo)*self.weights["fundamental_loss"]
+            octave=single_note_loss(-19, geo, i_note=1)*self.weights["octave_loss"]
 
             notes=geo.get_cadsd().get_notes()
             tuning_loss=0
             volume_loss=0
 
-            if len(notes)>2:
-                for ix, note in notes[2:].iterrows():
+            start_index=1
+            if self.add_octave:
+                start_index+=1
+            if len(notes)>start_index:
+                for ix, note in notes[start_index:].iterrows():
                     f1=math.log(note["freq"],2)
                     closest_target_index=np.argmin([abs(x-f1) for x in self.target_peaks])
                     f2=self.target_peaks[closest_target_index]
                     tuning_loss += math.sqrt(abs(f1-f2))
                     volume_loss += math.sqrt(1/(note["impedance"]/1e6))
-            tuning_loss*=4
-            volume_loss*=2
-            
-            n_note_loss=max(5-len(notes), 0)*5
 
-            d_loss = diameter_loss(geo)*0.1
+            tuning_loss*=self.weights["tuning_loss"]
+            volume_loss*=self.weights["volume_loss"]
+            
+            n_note_loss=max(5-len(notes), 0)*self.weights["n_note_loss"]
+
+            d_loss = diameter_loss(geo)*self.weights["diameter_loss"]
 
             loss={
                 # "tuning_loss": tuning_loss,
